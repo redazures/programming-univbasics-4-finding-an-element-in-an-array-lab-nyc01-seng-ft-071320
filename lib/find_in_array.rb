5f3ea2cfde6b14814cc6dd480abe8e681762349b
james wu <jwu_nyc@yahoo.com>
2020-06-27 01:55:56 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,20 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  win=nil
+  counter=0
+  while counter<array.length
+    if array[counter]==value_to_find
+      win=counter
+    end
+    counter+=1
+    puts counter
+  end
+  win
 end
+
+scale = [2,3,4,5]
+# scale.each do |element|
+#   puts element==2
+#   puts scale[element]
+# end
+puts find_element_index(scale,5)
